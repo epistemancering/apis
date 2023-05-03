82f3b3da23e9a7e0b0dba2c21c207e783c1fcafb
Austin Henrie <nitsua15@gmail.com>
2023-05-02 23:43:08 -0600

    first commit

---
+++ swapi/main.js
@@ -0,0 +1,19 @@
+function list() {
+    console.log("button clicked")
+    axios.get("https://swapi.dev/api/planets/?search=alderaan").then(function(response) {
+        console.log(response)
+        for (let index in response.data.results[0].residents) {
+            console.log(index)
+            axios.get(response.data.results[0].residents[index]).then(function(response) {
+                console.log(response)
+                let resident = document.createElement("h2")
+                console.log(resident)
+                resident.textContent = response.data.name
+                console.log(resident)
+                document.body.appendChild(resident)
+            })
+        }
+    })
+}
+let button = document.querySelector("button")
+button.addEventListener("click", list)
